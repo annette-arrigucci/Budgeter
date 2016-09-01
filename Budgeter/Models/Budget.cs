a6ca3926831e1665adea15965f1c16f23d2b957b
annette-arrigucci <annette.arrigucci@gmail.com>
2016-09-01 11:56:18 -0400

    added some classes for Models

---
+++ Budgeter/Models/Budget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Budgeter.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        public string Name { get; set; }
+        [Required]
+        public string HouseholdId { get; set; }
+
+        public Budget()
+        {
+            this.BudgetItems = new HashSet<BudgetItem>();
+        }
+
+        public virtual ICollection<BudgetItem> BudgetItems { get; set;}
+    }
+}
---
+++ Budgeter/Models/BudgetItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Budgeter.Models
+{
+    public class BudgetItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int BudgetId { get; set; }
+        [Required]
+        [Range(typeof(decimal), "0.00","1000000.00")]
+        public Decimal Amount { get; set; }
+        [Range(0,365)]
+        public int AnnualFrequency { get; set; }
+    }
+}

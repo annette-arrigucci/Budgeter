50cf11379dfad95e5285613127c4591521a23274
annette-arrigucci <annette.arrigucci@gmail.com>
2016-09-08 10:02:27 -0400

    added custom Authorize attribute

--- Budgeter/Models/IdentityModels.cs
+++ Budgeter/Models/IdentityModels.cs
@@ -5,6 +5,13 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.Security.Principal;
+using System.Linq;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace Budgeter.Models
 {
@@ -20,6 +27,8 @@ namespace Budgeter.Models
         [Display(Name = "Household")]
         public int? HouseholdId { get; set; }
 
+        public virtual Household Household { get; set; }
+
         public ApplicationUser()
         {
             this.Transactions = new HashSet<Transaction>();
@@ -32,9 +41,66 @@ namespace Budgeter.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim("HouseholdId", HouseholdId.ToString()));
+
             return userIdentity;
         }
     }
+
+    public static class Extensions
+    {
+        public static int? GetHouseholdId(this IIdentity user)
+        {
+            var claimsIdentity = (ClaimsIdentity)user;
+            var HouseholdClaim = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "HouseholdId");
+
+            if (HouseholdClaim != null)
+                return Int32.Parse(HouseholdClaim.Value);
+            else
+                return null;
+        }
+
+        public static bool IsInHousehold(this IIdentity user)
+        {
+            var cUser = (ClaimsIdentity)user;
+            var hid = cUser.Claims.FirstOrDefault(c => c.Type == "HouseholdId");
+            return (hid != null && !string.IsNullOrEmpty(hid.Value));
+        }
+
+        public static async Task RefreshAuthentication(this HttpContextBase context, ApplicationUser user)
+        {
+            context.GetOwinContext().Authentication.SignOut();
+            await context.GetOwinContext().Get<ApplicationSignInManager>().SignInAsync(user, isPersistent: false, rememberBrowser: false);
+        }
+    }
+
+    public class AuthorizeHouseholdRequired : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var isAuthorized = base.AuthorizeCore(httpContext);
+            if (!isAuthorized)
+            {
+                return false;
+            }
+            return httpContext.User.Identity.IsInHousehold();
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Household", action = "Index" }));
+            }
+            
+        }
+    }
+
+    
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
